10117aab05f286b8bd5bace429772fe9f71c8274
Inês Oliveira <inesfpoliveira@gmail.com>
2020-06-16 01:09:30 +0100

    remove multiple entries

--- src/Components/Qualifiers.js
+++ src/Components/Qualifiers.js
@@ -12,62 +12,6 @@ const qualifiers = [
         count: 1,
         flag: 'https://i.imgur.com/AvcDSgg.png',
         pic: 'https://i.imgur.com/dXhxeIW.png'
-    },
-    {
-        name: 'Yuga Mikado',
-        world: 'Dragon World',
-        count: 1,
-        flag: 'https://i.imgur.com/epIFZUq.png',
-        pic: 'https://i.imgur.com/D1yHXbw.png'
-    },
-    {
-        name: 'Ranma Kakogawa',
-        world: 'Lost World',
-        count: 1,
-        flag: 'https://i.imgur.com/AvcDSgg.png',
-        pic: 'https://i.imgur.com/dXhxeIW.png'
-    },
-    {
-        name: 'Yuga Mikado',
-        world: 'Dragon World',
-        count: 1,
-        flag: 'https://i.imgur.com/epIFZUq.png',
-        pic: 'https://i.imgur.com/D1yHXbw.png'
-    },
-    {
-        name: 'Ranma Kakogawa',
-        world: 'Lost World',
-        count: 1,
-        flag: 'https://i.imgur.com/AvcDSgg.png',
-        pic: 'https://i.imgur.com/dXhxeIW.png'
-    },
-    {
-        name: 'Yuga Mikado',
-        world: 'Dragon World',
-        count: 1,
-        flag: 'https://i.imgur.com/epIFZUq.png',
-        pic: 'https://i.imgur.com/D1yHXbw.png'
-    },
-    {
-        name: 'Ranma Kakogawa',
-        world: 'Lost World',
-        count: 1,
-        flag: 'https://i.imgur.com/AvcDSgg.png',
-        pic: 'https://i.imgur.com/dXhxeIW.png'
-    },
-    {
-        name: 'Yuga Mikado',
-        world: 'Dragon World',
-        count: 1,
-        flag: 'https://i.imgur.com/epIFZUq.png',
-        pic: 'https://i.imgur.com/D1yHXbw.png'
-    },
-    {
-        name: 'Ranma Kakogawa',
-        world: 'Lost World',
-        count: 1,
-        flag: 'https://i.imgur.com/AvcDSgg.png',
-        pic: 'https://i.imgur.com/dXhxeIW.png'
     }
 ];
 
